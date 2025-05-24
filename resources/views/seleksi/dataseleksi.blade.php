2e4f924edd1459ff08d2d6b51072be606d81e8a2
umarovk <umarov.studio@gmail.com>
2025-05-24 10:15:45 +0700

    fix : btn seleksi dan export

--- resources/views/seleksi/dataseleksi.blade.php
+++ resources/views/seleksi/dataseleksi.blade.php
@@ -8,6 +8,16 @@
                     <h1 class="fw-light text-primary mb-0 fs-3">Data Seleksi Siswa Baru</h1>
                 </div>
 
+                <div class="col-md-4 text-md-end mt-3 mt-md-0">
+                    <!-- Add any global action button here if needed -->
+                    <a
+                        href="{{ route('seleksi.export') }}"
+                        class="btn btn-success transaction-action w-50"
+                    >
+                        Download Data <i class="bi bi-download"></i>
+                    </a>
+                </div>
+
             </div>
 
             <div class="card shadow-sm border-0 rounded-3 mb-4">
